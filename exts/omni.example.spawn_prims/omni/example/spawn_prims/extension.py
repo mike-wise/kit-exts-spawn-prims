7d0db19b78635c7f283ca7b9e1d49893a5e9529f
MikeWise2718 <mwise@microsoft.com>
2023-05-30 13:20:14 +0200

    async experiements

--- exts/omni.example.spawn_prims/omni/example/spawn_prims/extension.py
+++ exts/omni.example.spawn_prims/omni/example/spawn_prims/extension.py
@@ -3,8 +3,9 @@ import omni.ui as ui
 import omni.kit.commands as okc
 import omni.usd
 import os
+import time
 from pxr import Gf, Sdf, Usd, UsdGeom, UsdShade
-from .ovut import MatMan, SphereMeshFactory, SphereFlakeFactory
+from .ovut import MatMan, SphereMeshFactory, SphereFlakeFactory, delete_if_exists
 
 # fflake8: noqa
 
@@ -18,17 +19,25 @@ class PrimsExtension(omni.ext.IExt):
     _stage = None
     _total_quads: int = 0
     _matman: MatMan = None
+    _floor_xdim = 5
+    _floor_zdim = 5
 
-    def make_initial_scene(self):
+    def setup_environment(self, force: bool = False):
         ppathstr = "/World/Floor"
+        if force:
+            delete_if_exists(ppathstr)
+
         prim_path_sdf = Sdf.Path(ppathstr)
+
         prim: Usd.Prim = self._stage .GetPrimAtPath(prim_path_sdf)
         if not prim.IsValid():
             okc.execute('CreateMeshPrimWithDefaultXform',	prim_type="Plane", prim_path=ppathstr)
+            self._floor_xdim = 4 + self._nsf_x
+            self._floor_zdim = 4 + self._nsf_z
             okc.execute('TransformMultiPrimsSRTCpp',
                         count=1,
                         paths=[ppathstr],
-                        new_scales=[5, 1, 5])
+                        new_scales=[self._floor_xdim, 1, self._floor_zdim])
             baseurl = 'https://omniverse-content-production.s3.us-west-2.amazonaws.com'
             okc.execute('CreateDynamicSkyCommand',
                         sky_url=f'{baseurl}/Assets/Skies/2022_1/Skies/Dynamic/CumulusLight.usd',
@@ -41,12 +50,12 @@ class PrimsExtension(omni.ext.IExt):
             print(f"ensure_stage:{self._stage}")
             UsdGeom.SetStageUpAxis(self._stage, UsdGeom.Tokens.y)
             self._total_quads = 0
-            self.make_initial_scene()
+            self.setup_environment()
 
-    def create_billboard(self):
+    def create_billboard(self, primpath: str):
         UsdGeom.SetStageUpAxis(self._stage, UsdGeom.Tokens.y)
 
-        billboard = UsdGeom.Mesh.Define(self._stage, "/World/Billboard")
+        billboard = UsdGeom.Mesh.Define(self._stage, primpath)
         billboard.CreatePointsAttr([(-430, -145, 0), (430, -145, 0), (430, 145, 0), (-430, 145, 0)])
         billboard.CreateFaceVertexCountsAttr([4])
         billboard.CreateFaceVertexIndicesAttr([0, 1, 2, 3])
@@ -72,6 +81,12 @@ class PrimsExtension(omni.ext.IExt):
         self._sf_spawn_but: ui.Button = None
         self._sf_nlat_but: ui.Button = None
         self._sf_nlng_but: ui.Button = None
+        self._matbox: ui.ComboBox = None
+        self._curprim = "Sphere"
+        self._prims = ["Sphere", "Cube", "Cone", "Torus", "Cylinder", "Plane", "Disk", "Capsule"]
+        self._prims_created = []
+        self._nsf_x = 1
+        self._nsf_z = 1
 
         with self._window.frame:
             with ui.VStack():
@@ -79,50 +94,80 @@ class PrimsExtension(omni.ext.IExt):
                 def on_click_billboard():
                     self.ensure_stage()
 
-                    billboard = self.create_billboard()
+                    primpath = f"/World/Billboard_{self._count}"
+                    billboard = self.create_billboard(primpath)
+                    self._prims_created.append(primpath)
 
                     material = self.get_curmat_mat()
                     UsdShade.MaterialBindingAPI(billboard).Bind(material)
-                    print(billboard)
-
-                    print(f"billboard clicked (cwd:{os.getcwd()})")
 
                 def on_click_spheremesh():
                     self.ensure_stage()
 
-                    sm = SphereMeshFactory(self._matman)
+                    sm = SphereMeshFactory(self._matman, self._sf_nlat, self._sf_nlng)
 
                     matname = self.get_curmat_name()
                     sz = 50
                     cpt = Gf.Vec3f(0, sz, 0)
                     primpath = f"/World/SphereMesh_{self._count}"
                     self._count += 1
-                    sm.Create(primpath, matname, cpt, sz, 8, 8)
-                    print(f"spheremesh clicked (cwd:{os.getcwd()})")
+                    sm.CreateMesh(primpath, matname, cpt, sz)
+                    self._prims_created.append(primpath)
 
                 def on_click_sphereflake():
                     self.ensure_stage()
 
-                    sff = SphereFlakeFactory(self._matman)
+                    sff = SphereFlakeFactory(self._matman,  nlat=self._sf_nlat, nlong=self._sf_nlng)
 
                     matname = self.get_curmat_name()
                     sz = 50
                     cpt = Gf.Vec3f(0, sz, 0)
                     primpath = f"/World/SphereFlake_{self._count}"
+
                     self._count += 1
                     depth = self._sf_depth
-                    sff.Create(primpath, matname, depth, depth, cpt, cpt, sz, nlat=self._sf_nlat, nlong=self._sf_nlng)
+                    sff.Generate(primpath, matname, depth, depth, cpt, sz)
 
-                    okc.execute('TransformMultiPrimsSRTCpp',
-                                count=1,
-                                paths=[primpath],
-                                new_scales=[1, 1, 1],
-                                new_translations=[0, 0, 0])
+                    self._prims_created.append(primpath)
+                    UpdateNQuads()
+
+                def gensflakes():
+                    sff = SphereFlakeFactory(self._matman,  nlat=self._sf_nlat, nlong=self._sf_nlng)
+
+                    matname = self.get_curmat_name()
+                    sz = 50
+                    cpt = Gf.Vec3f(0, sz, 0)
+                    depth = self._sf_depth
+                    ixoff = (float(self._nsf_x) / 2) - 0.5
+                    izoff = (float(self._nsf_z) / 2) - 0.5
+                    print(f"ixoff:{ixoff} izoff:{izoff} type(ixoff):{type(ixoff)} type(izoff):{type(izoff)}")
+                    for ix in range(self._nsf_x):
+                        for iz in range(self._nsf_z):
+                            self._count += 1
+                            primpath = f"/World/SphereFlake_{self._count}"
+                            cpt = Gf.Vec3f((ix-ixoff)*sz*3, sz, (iz-izoff)*sz*3)
+                            sff.Generate(primpath, matname, depth, depth, cpt, sz)
+                            self._prims_created.append(primpath)
+
+                def on_click_multi_sphereflake():
+                    self.ensure_stage()
+                    self.setup_environment(force=True)
+
+                    start_time = time.time()
+                    gensflakes()
+                    elap = time.time() - start_time
+
+                    nflakes = self._nsf_x * self._nsf_z
+
+                    self._statuslable.text = f"{nflakes} flakes took elapsed: {elap:.2f} s"
+
+                    UpdateNQuads()
                     print(f"sphereflake clicked (cwd:{os.getcwd()})")
 
-                def on_click(primtype):
+                def spawnprim(primtype):
                     self.ensure_stage()
                     primpath = f"/World/{primtype}_{self._count}"
+                    self._prims_created.append(primpath)
                     okc.execute('CreateMeshPrimWithDefaultXform',	prim_type=primtype, prim_path=primpath)
 
                     material = self.get_curmat_mat()
@@ -133,8 +178,6 @@ class PrimsExtension(omni.ext.IExt):
                                 paths=[primpath],
                                 new_scales=[1, 1, 1],
                                 new_translations=[0, 50, 0])
-
-                    print(f"on click binding:{material}")
                     prim: Usd.Prim = self._stage.GetPrimAtPath(primpath)
                     UsdShade.MaterialBindingAPI(prim).Bind(material)
 
@@ -144,47 +187,92 @@ class PrimsExtension(omni.ext.IExt):
                         self._sf_depth = 0
                     self._sf_depth_but.text = f"Depth:{self._sf_depth}"
                     UpdateNQuads()
+                    UpdateMQuads()
 
                 def on_click_nlat():
                     self._sf_nlat += 1
-                    if self._sf_nlat > 10:
+                    if self._sf_nlat > 16:
                         self._sf_nlat = 2
                     self._sf_nlat_but.text = f"Nlat:{self._sf_nlat}"
                     UpdateNQuads()
+                    UpdateMQuads()
 
                 def on_click_nlng():
                     self._sf_nlng += 1
-                    if self._sf_nlng > 10:
+                    if self._sf_nlng > 16:
                         self._sf_nlng = 3
                     self._sf_nlng_but.text = f"Nlng:{self._sf_nlng}"
                     UpdateNQuads()
+                    UpdateMQuads()
+
+                def on_click_sfx():
+                    self._nsf_x += 1
+                    if self._nsf_x > 16:
+                        self._nsf_x = 1
+                    self._nsf_x_but.text = f"SF - x:{self._nsf_x}"
+                    UpdateMQuads()
+
+                def on_click_sfz():
+                    self._nsf_z += 1
+                    if self._nsf_z > 16:
+                        self._nsf_z = 1
+                    self._nsf_z_but.text = f"SF - z:{self._nsf_z}"
+                    UpdateMQuads()
+
+                def on_click_spawnprim():
+                    spawnprim(self._curprim)
+
+                def on_click_clearprims():
+                    for primpath in self._prims_created:
+                        delete_if_exists(primpath)
+                    self._prims_created = []
+
+                def on_click_changeprim():
+                    idx = self._prims.index(self._curprim)
+                    idx += 1
+                    if idx >= len(self._prims):
+                        idx = 0
+                    self._curprim = self._prims[idx]
+                    self._sf_primtospawn_but.text = f"{self._curprim}"
 
                 def UpdateNQuads():
-                    nquads = SphereFlakeFactory.CalcQuads(self._sf_depth, 8,  self._sf_nlat, self._sf_nlng)
-                    self._sf_spawn_but.text = f"Spawn ShereFlake ({nquads} quads)"
+                    ntris = SphereFlakeFactory.CalcTris(self._sf_depth, 8,  self._sf_nlat, self._sf_nlng)
+                    elap = SphereFlakeFactory.GetLastGenTime()
+                    self._sf_spawn_but.text = f"Spawn ShereFlake\n{ntris} tris\nGenElap: {elap:.2f} s"
 
-                ui.Button("Spawn Cube", clicked_fn=lambda: on_click("Cube"))
-                ui.Button("Spawn Cone", clicked_fn=lambda: on_click("Cone"))
-                ui.Button("Spawn Cylinder", clicked_fn=lambda: on_click("Cylinder"))
-                ui.Button("Spawn Disk", clicked_fn=lambda: on_click("Disk"))
-                ui.Button("Spawn Plane", clicked_fn=lambda: on_click("Plane"))
-                ui.Button("Spawn Sphere", clicked_fn=lambda: on_click("Sphere"))
-                ui.Button("Spawn Torus", clicked_fn=lambda: on_click("Torus"))
+                def UpdateMQuads():
+                    ntris = SphereFlakeFactory.CalcTris(self._sf_depth, 8,  self._sf_nlat, self._sf_nlng)
+                    tottris = ntris*self._nsf_x*self._nsf_z
+                    self._msf_spawn_but.text = f"Multi ShereFlake\n{tottris} tris"
+
+                ui.Button("Clear Prims", clicked_fn=lambda: on_click_clearprims())
+                with ui.HStack():
+                    self._sf_spawn_but = ui.Button("Spawn Prim", clicked_fn=lambda: on_click_spawnprim())
+                    self._sf_primtospawn_but = ui.Button(f"{self._curprim}", clicked_fn=lambda: on_click_changeprim())
                 ui.Button("Spawn Billboard", clicked_fn=lambda: on_click_billboard())
                 ui.Button("Spawn ShereMesh", clicked_fn=lambda: on_click_spheremesh())
-                with ui.HStack():
-                    self._sf_spawn_but = ui.Button("Spawn ShereFlake", clicked_fn=lambda: on_click_sphereflake())
-                    self._sf_depth_but = ui.Button(f"Depth:{self._sf_depth}", clicked_fn=lambda: on_click_sfdepth())
-                    with ui.VStack():
-                        self._sf_nlat_but = ui.Button(f"Nlat:{self._sf_nlat}", clicked_fn=lambda: on_click_nlat())
-                        self._sf_nlng_but = ui.Button(f"Nlng:{self._sf_nlng}", clicked_fn=lambda: on_click_nlng())
-
+                with ui.VStack():
+                    with ui.HStack():
+                        self._sf_spawn_but = ui.Button("Spawn ShereFlake", clicked_fn=lambda: on_click_sphereflake())
+                        self._sf_depth_but = ui.Button(f"Depth:{self._sf_depth}", clicked_fn=lambda: on_click_sfdepth())
+                        with ui.VStack():
+                            self._sf_nlat_but = ui.Button(f"Nlat:{self._sf_nlat}", clicked_fn=lambda: on_click_nlat())
+                            self._sf_nlng_but = ui.Button(f"Nlng:{self._sf_nlng}", clicked_fn=lambda: on_click_nlng())
+                            
+                    with ui.HStack():
+                        self._msf_spawn_but = ui.Button("Multi ShereFlake", clicked_fn=
+                                                        lambda: on_click_multi_sphereflake())
+                        with ui.VStack():
+                            self._nsf_x_but = ui.Button(f"SF x: {self._nsf_x}", clicked_fn=lambda: on_click_sfx())
+                            self._nsf_z_but = ui.Button(f"SF z: {self._nsf_z}", clicked_fn=lambda: on_click_sfz())
                 UpdateNQuads()
+                UpdateMQuads()
 
                 idx = self._matkeys.index(self._current_material)
                 if idx < 0:
                     idx = 0
                 self._matbox = ui.ComboBox(idx, *self._matkeys).model
+                self._statuslable = ui.Label("Status: Ready")
 
     def get_curmat_mat(self):
         idx = self._matbox.get_item_value_model().as_int
--- exts/omni.example.spawn_prims/omni/example/spawn_prims/ovut.py
+++ exts/omni.example.spawn_prims/omni/example/spawn_prims/ovut.py
@@ -1,12 +1,14 @@
 import omni.ext
-import omni.ui as ui
 import omni.kit.commands as okc
 import omni.usd
 import os
 import math
 import time
-from pxr import Gf, Sdf, Usd, UsdGeom, UsdShade
+import asyncio
+import numpy as np
+from pxr import Gf, Sdf, Usd, UsdGeom, UsdShade, Vt
 from typing import Tuple, List
+
 
 def delete_if_exists(primpath: str) -> None:
     stage = omni.usd.get_context().get_stage()
@@ -22,15 +24,17 @@ def cross_product(v1: Gf.Vec3f, v2: Gf.Vec3f) -> Gf.Vec3f:
     return rv
 
 
-class SphereMeshFactory():
+class SphereMeshFactoryV1():
 
     _show_normals = False
 
-    def __init__(self, matman, show_normals=False) -> None:
+    def __init__(self, matman, nlat: int, nlng: int, show_normals=False) -> None:
         self._stage = omni.usd.get_context().get_stage()
         self._matman = matman
         self._show_normals = show_normals
         self._total_quads = 0
+        self._nlat = nlat
+        self._nlong = nlng
         pass
 
     def MakeMarker(self, name: str, matname: str, cenpt: Gf.Vec3f, rad: float):
@@ -48,11 +52,13 @@ class SphereMeshFactory():
         mtl = self._matman.GetMaterial(matname)
         UsdShade.MaterialBindingAPI(prim).Bind(mtl)
 
-    def Create(self, name: str, matname: str, cenpt: Gf.Vec3f, radius: float, nlat: int, nlong: int):
+    def CreateMesh(self, name: str, matname: str, cenpt: Gf.Vec3f, radius: float):
         # This will create nlat*nlog quads or twice that many triangles
         # it will need nlat+1 vertices in the latitude direction and nlong vertices in the longitude direction
         # so a total of (nlat+1)*(nlong) vertices
         spheremesh = UsdGeom.Mesh.Define(self._stage, name)
+        nlat = self._nlat
+        nlong = self._nlong
         vtxcnt = int(0)
         pts = []
         nrm = []
@@ -122,6 +128,168 @@ class SphereMeshFactory():
         return spheremesh
 
 
+class SphereMeshFactory():
+
+    _show_normals = False
+
+    def __init__(self, matman, nlat: int, nlng: int, show_normals: bool=False, do_text_coords: bool=False) -> None:
+        self._stage = omni.usd.get_context().get_stage()
+        self._matman = matman
+        self._show_normals = show_normals
+        self._dotexcoords = do_text_coords
+        self._total_quads = 0
+        self._nlat = nlat
+        self._nlong = nlng
+        self._nquads = nlat*nlng
+        self._nverts = (nlat+1)*(nlng)
+        self._normbuf = np.zeros((self._nverts, 3), dtype=np.float32)
+        self._txtrbuf = np.zeros((self._nverts, 2), dtype=np.float32)
+        self._facebuf = np.zeros((self._nquads, 1), dtype=np.int32)
+        self._vidxbuf = np.zeros((self._nquads, 4), dtype=np.int32)
+        self.MakeArrays()
+
+    def MakeMarker(self, name: str, matname: str, cenpt: Gf.Vec3f, rad: float):
+        print(f"MakeMarker {name}  {cenpt} {rad}")
+        primpath = f"/World/markers/{name}"
+        delete_if_exists(primpath)
+        okc.execute('CreateMeshPrimWithDefaultXform', prim_type="Sphere", prim_path=primpath)
+        sz = rad/100
+        okc.execute('TransformMultiPrimsSRTCpp',
+                    count=1,
+                    paths=[primpath],
+                    new_scales=[sz, sz, sz],
+                    new_translations=[cenpt[0], cenpt[1], cenpt[2]])
+        prim: Usd.Prim = self._stage.GetPrimAtPath(primpath)
+        mtl = self._matman.GetMaterial(matname)
+        UsdShade.MaterialBindingAPI(prim).Bind(mtl)
+
+    def MakeArrays(self):
+        nlat = self._nlat
+        nlong = self._nlong
+        for i in range(nlat):
+            offset = i * nlong
+            for j in range(nlong):
+                if j < nlong - 1:
+                    i1 = offset+j
+                    i2 = offset+j+1
+                    i3 = offset+j+nlong+1
+                    i4 = offset+j+nlong
+                else:
+                    i1 = offset+j
+                    i2 = offset
+                    i3 = offset+nlong
+                    i4 = offset+j+nlong
+                vidx = i*nlong+j
+                self._facebuf[vidx] = 4
+                self._vidxbuf[vidx] = [i1, i2, i3, i4]
+
+        polegap = 0.01  # prevents the vertices from being exactly on the poles
+        for i in range(nlat+1):
+            theta = polegap + (i * (math.pi-2*polegap) / float(nlat))
+            st = math.sin(theta)
+            ct = math.cos(theta)
+            for j in range(nlong):
+                phi = j * 2 * math.pi / float(nlong)
+                sp = math.sin(phi)
+                cp = math.cos(phi)
+                nx = st*cp
+                ny = ct
+                nz = st*sp
+                nrmvek = Gf.Vec3f(nx, ny, nz)
+                vidx = i*nlong+j
+                self._normbuf[vidx] = nrmvek
+                self._txtrbuf[vidx] = (nx, ny)
+        #  print("MakeArrays done")
+
+    def ShowNormals(self, vertbuf):
+        nlat = self._nlat
+        nlong = self._nlong
+        for i in range(nlat+1):
+            for j in range(nlong):
+                vidx = i*nlong+j
+                ptname = f"ppt_{i}_{j}"
+                (x, y, z) = vertbuf[vidx]
+                (nx, ny, nz) = self._nromtbuf[vidx]
+                pt = Gf.Vec3f(x, y, z)
+                npt = Gf.Vec3f(x+nx, y+ny, z+nz)
+                nmname = f"npt_{i}_{j}"
+                self.MakeMarker(ptname, "red", pt, 1)
+                self.MakeMarker(nmname, "blue", npt, 1)
+
+    def CreateMesh(self, name: str, matname: str, cenpt: Gf.Vec3f, radius: float):
+        # This will create nlat*nlog quads or twice that many triangles
+        # it will need nlat+1 vertices in the latitude direction and nlong vertices in the longitude direction
+        # so a total of (nlat+1)*(nlong) vertices
+
+        spheremesh = UsdGeom.Mesh.Define(self._stage, name)
+
+        # note that vertbuf is local to this function allowing it to be changed in a multithreaded environment
+        vertbuf = self._normbuf*radius + cenpt
+
+        if self._show_normals:
+            self.ShowNormals(vertbuf)
+
+        if self._dotexcoords:
+            texCoords = UsdGeom.PrimvarsAPI(spheremesh).CreatePrimvar("st",
+                                                                      Sdf.ValueTypeNames.TexCoord2fArray,
+                                                                      UsdGeom.Tokens.varying)
+            texCoords.Set(Vt.Vec2fArray.FromNumpy(self._txtrbuf))
+
+        spheremesh.CreatePointsAttr(Vt.Vec3dArray.FromNumpy(vertbuf))
+        spheremesh.CreateNormalsAttr(Vt.Vec3dArray.FromNumpy(self._normbuf))
+        spheremesh.CreateFaceVertexCountsAttr(Vt.IntArrayFromBuffer(self._facebuf))
+        spheremesh.CreateFaceVertexIndicesAttr(Vt.IntArrayFromBuffer(self._vidxbuf))
+
+        mtl = self._matman.GetMaterial(matname)
+        UsdShade.MaterialBindingAPI(spheremesh).Bind(mtl)
+
+        self._total_quads += self._nquads  # face vertex counts
+
+        return None
+
+    async def CreateVertBuf(self, radius, cenpt):
+        vertbuf = self._normbuf*radius + cenpt
+        return vertbuf
+
+    async def CreateStuff(self, spheremesh, vertbuf, normbuf, facebuf, vidxbuf):
+        spheremesh.CreatePointsAttr(Vt.Vec3dArray.FromNumpy(vertbuf))
+        spheremesh.CreateNormalsAttr(Vt.Vec3dArray.FromNumpy(normbuf))
+        spheremesh.CreateFaceVertexCountsAttr(Vt.IntArrayFromBuffer(facebuf))
+        spheremesh.CreateFaceVertexIndicesAttr(Vt.IntArrayFromBuffer(vidxbuf))
+        return
+
+    async def CreateMeshAsync(self, name: str, matname: str, cenpt: Gf.Vec3f, radius: float):
+        # This will create nlat*nlog quads or twice that many triangles
+        # it will need nlat+1 vertices in the latitude direction and nlong vertices in the longitude direction
+        # so a total of (nlat+1)*(nlong) vertices
+
+        spheremesh = UsdGeom.Mesh.Define(self._stage, name)
+
+        # note that vertbuf is local to this function allowing it to be changed in a multithreaded environment
+        vertbuf = await self.CreateVertBuf(radius, cenpt)
+
+        if self._show_normals:
+            self.ShowNormals(vertbuf)
+
+        if self._dotexcoords:
+            texCoords = UsdGeom.PrimvarsAPI(spheremesh).CreatePrimvar("st",
+                                                                      Sdf.ValueTypeNames.TexCoord2fArray,
+                                                                      UsdGeom.Tokens.varying)
+            texCoords.Set(Vt.Vec2fArray.FromNumpy(self._txtrbuf))
+
+        await self.CreateStuff(spheremesh, vertbuf, self._normbuf, self._facebuf, self._vidxbuf)
+
+        mtl = self._matman.GetMaterial(matname)
+        UsdShade.MaterialBindingAPI(spheremesh).Bind(mtl)
+
+        self._total_quads += self._nquads  # face vertex counts
+
+        return None
+
+
+latest_sf_gen_time = 0
+
+
 class SphereFlakeFactory():
 
     org = Gf.Vec3f(0, 0, 0)
@@ -129,11 +297,12 @@ class SphereFlakeFactory():
     yax = Gf.Vec3f(0, 1, 0)
     zax = Gf.Vec3f(0, 0, 1)
 
-    def __init__(self, matman) -> None:
+    def __init__(self, matman,  nlat, nlong) -> None:
         self._stage = omni.usd.get_context().get_stage()
         self._matman = matman
-        self._total_quads = 0
-        self._smf = SphereMeshFactory(self._matman, show_normals=False)
+        self._nlat = nlat
+        self._nlng = nlong
+        self._smf = SphereMeshFactory(self._matman,  nlat, nlong, show_normals=False)
 
     @staticmethod
     def CalcQuads(depth: int, nring: int, nlat: int, nlng: int):
@@ -143,31 +312,54 @@ class SphereFlakeFactory():
             nquads = nspheres * nlat * nlng
             totquads += nquads
         return totquads
-    
-    def CalcTris(self, depth: int, nring: int, nlat: int, nlng: int):
-        totquads = self.CalcQuads(depth, nring, nlat, nlng)
+
+    @staticmethod
+    def CalcTris(depth: int, nring: int, nlat: int, nlng: int):
+        totquads = SphereFlakeFactory.CalcQuads(depth, nring, nlat, nlng)
         return totquads * 2
 
-    def Create(self, sphflkname: str, matname: str, mxdepth: int, depth: int, basept: Gf.Vec3f,
-               cenpt: Gf.Vec3f, rad: float, nlat: int = 8, nlong: int = 8):
+    @staticmethod
+    def GetLastGenTime():
+        global latest_sf_gen_time
+        return latest_sf_gen_time
+
+    def Generate(self, sphflkname: str, matname: str, mxdepth: int, depth: int, cenpt: Gf.Vec3f, rad: float):
+
+        global latest_sf_gen_time
+
+        self._start_time = time.time()
+        self._total_quads = 0
 
         self._depth = depth
-        self._nlat = nlat
-        self._nlng = nlong
         self._nring = 8
-
         delete_if_exists(sphflkname)
-        if depth == mxdepth:
-            self._total_quads = 0
-            self._start_time = time.time()
 
         xformPrim = UsdGeom.Xform.Define(self._stage, sphflkname)
         UsdGeom.XformCommonAPI(xformPrim).SetTranslate((0, 0, 0))
         UsdGeom.XformCommonAPI(xformPrim).SetRotate((0, 0, 0))
 
+        basept = cenpt
+        self.Create(sphflkname, matname, mxdepth, depth, basept, cenpt, rad)
+
+        elap = time.time() - self._start_time
+        print(f"GenerateSF {sphflkname} {matname} {depth} {cenpt} totquads:{self._total_quads} in {elap:.3f} secs")
+
+        latest_sf_gen_time = elap
+
+    def Create(self, sphflkname: str, matname: str, mxdepth: int, depth: int, basept: Gf.Vec3f,
+               cenpt: Gf.Vec3f, rad: float):
+
+        # xformPrim = UsdGeom.Xform.Define(self._stage, sphflkname)
+        # UsdGeom.XformCommonAPI(xformPrim).SetTranslate((0, 0, 0))
+        # UsdGeom.XformCommonAPI(xformPrim).SetRotate((0, 0, 0))
+
         meshname = sphflkname + "/SphereMesh"
 
-        self._smf.Create(meshname, matname, cenpt,  rad, nlat, nlong)
+        # spheremesh = UsdGeom.Mesh.Define(self._stage, meshname)
+
+        asyncio.ensure_future(self._smf.CreateMeshAsync(meshname, matname, cenpt,  rad))
+        # self._smf.CreateMesh(meshname, matname, cenpt,  rad)
+        # self._smf.CreateMesh(meshname+"1", matname, cenpt,  rad)
 
         offvek = cenpt - basept
         len = offvek.GetLength()
@@ -186,19 +378,15 @@ class SphereFlakeFactory():
             lzax = self.zax
 
         if depth > 0:
+            nrad = rad / 4
             for i in range(self._nring):
                 theta = i * 2 * math.pi / self._nring
                 x = rad * math.sin(theta)
                 y = 0
                 z = rad * math.cos(theta)
-                nrad = rad / 4
                 npt = x*lxax + y*lyax + z*lzax
                 subname = f"{sphflkname}/sf_{i}"
-                self.Create(subname, matname, mxdepth, depth-1, cenpt, cenpt+1.25*npt, nrad, nlat=nlat, nlong=nlong)
-
-        if depth == mxdepth:
-            elap = time.time() - self._start_time
-            print(f"Create {sphflkname} {matname} {depth} {cenpt} totquads:{self._total_quads} in {elap:.3f} secs")
+                self.Create(subname, matname, mxdepth, depth-1, cenpt, cenpt+1.25*npt, nrad)
 
 
 class MatMan():
